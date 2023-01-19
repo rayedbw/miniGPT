1434b9be2fbdc3ae2cc2e36c8427483eda942c01
Md Rayed Bin Wahed <rayed.bin.wahed@g.bracu.ac.bd>
2023-01-19 23:36:05 +0800

    Implement Transformer decoder

--- bigram.py
+++ bigram.py
@@ -7,13 +7,15 @@ from torch.nn import functional as F
 
 # Hyperparams
 BATCH_SIZE = 128
-SEQ_LEN = 16
+SEQ_LEN = 256
 MAX_ITER = int(1e5)
 EVAL_INTERVAL = int(1e4)
 EVAL_ITER = int(1e3)
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-EMBED_DIM = 32
-ATTN_OUT = 16
+EMBED_DIM = 512
+NUM_HEADS = 8
+NUM_BLOCKS = 6
+FEED_FORWARD_MULTIPLIER = 4
 
 # For reproducibility
 torch.manual_seed(1337)
@@ -29,7 +31,8 @@ if not file_path.exists():
                 f.write(chunk)
     print(f"Saving to {file_path}")
 else:
-    print("Dataset exists")
+    # print("Dataset exists")
+    pass
 
 # Load dataset
 with open(file_path) as f:
@@ -82,47 +85,110 @@ def estimate_loss():
 class SelfAttentionHead(nn.Module):
     """Self Attention Head"""
 
-    def __init__(self) -> None:
+    def __init__(self, head_dim) -> None:
         super().__init__()
-        self.query = nn.Linear(EMBED_DIM, ATTN_OUT, bias=False)
-        self.key = nn.Linear(EMBED_DIM, ATTN_OUT, bias=False)
-        self.value = nn.Linear(EMBED_DIM, ATTN_OUT, bias=False)
+        self.head_dim = head_dim
+        self.query = nn.Linear(EMBED_DIM, head_dim, bias=False)
+        self.key = nn.Linear(EMBED_DIM, head_dim, bias=False)
+        self.value = nn.Linear(EMBED_DIM, head_dim, bias=False)
         self.register_buffer("tril", torch.tril(torch.ones((SEQ_LEN, SEQ_LEN))))
+        self.dropout = nn.Dropout()
 
     def forward(self, x):
-        B, T, C = x.shape
+        _, T, _ = x.shape
 
         q = self.query(x)  # B, T, H
         k = self.key(x)
         v = self.value(x)
 
-        attn_weights = (
-            q @ k.transpose(-2, -1) * ATTN_OUT**-0.5
+        allignment_scores = (
+            q @ k.transpose(-2, -1) * self.head_dim**-0.5
         )  # B, T, H @ B, H, T --> B, T, T
-        attn_weights = attn_weights.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
-        attn_weights = F.softmax(attn_weights, dim=-1)
+        allignment_scores_masked = allignment_scores.masked_fill(
+            self.tril[:T, :T] == 0, float("-inf")
+        )  # T because seq_len changes during inference
+        attn_weights = F.softmax(allignment_scores_masked, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+        attn_output = attn_weights @ v  # (B, T, T) @ (B, T, H) --> B, T, H
 
-        output = attn_weights @ v  # (B, T, T) @ (B, T, H) --> B, T, H
-        return output
+        return attn_output
+
+
+class MultiHeadAttention(nn.Module):
+    """Multi headed self attention"""
+
+    def __init__(self, embed_dim, num_heads) -> None:
+        super().__init__()
+        head_dim = embed_dim // num_heads
+        assert (
+            head_dim * num_heads == embed_dim
+        ), "embed_dim must be divisible by num_heads"
+        self.heads = nn.ModuleList(
+            [SelfAttentionHead(head_dim) for _ in range(num_heads)]
+        )
+        self.linear = nn.Linear(embed_dim, embed_dim)
+        self.dropout = nn.Dropout()
+
+    def forward(self, x):
+        x = torch.concat([head(x) for head in self.heads], dim=-1)
+        x = self.linear(x)
+        x = self.dropout(x)
+        return x
+
+
+class FeedForwardNetwork(nn.Module):
+    """Single layer MLP"""
+
+    def __init__(self, in_features) -> None:
+        super().__init__()
+        self.ffn = nn.Sequential(
+            nn.Linear(in_features, in_features * FEED_FORWARD_MULTIPLIER),
+            nn.ReLU(),
+            nn.Linear(in_features * FEED_FORWARD_MULTIPLIER, in_features),
+            nn.Dropout(),
+        )
+
+    def forward(self, x):
+        return self.ffn(x)
+
+
+class Block(nn.Module):
+    """Transformer block"""
+
+    def __init__(self, embed_dim, num_heads) -> None:
+        super().__init__()
+        self.mha = MultiHeadAttention(embed_dim, num_heads)
+        self.ffn = FeedForwardNetwork(embed_dim)
+        self.ln1 = nn.LayerNorm(embed_dim)
+        self.ln2 = nn.LayerNorm(embed_dim)
+
+    def forward(self, x):
+        x = self.mha(self.ln1(x)) + x
+        x = self.ffn(self.ln2(x)) + x
+        return x
 
 
 # Define bigram model
 class BigramLM(nn.Module):
     def __init__(self):
         super().__init__()
-        self.token_embedding = nn.Embedding(VOCAB_SIZE, EMBED_DIM)
-        self.positional_embedding = nn.Embedding(SEQ_LEN, EMBED_DIM)
-        self.self_attention_head = SelfAttentionHead()
-        self.linear = nn.Linear(ATTN_OUT, VOCAB_SIZE)
+        self.token_embed = nn.Embedding(VOCAB_SIZE, EMBED_DIM)
+        self.pos_embed = nn.Embedding(SEQ_LEN, EMBED_DIM)
+        self.blocks = nn.Sequential(
+            *[Block(EMBED_DIM, NUM_HEADS) for _ in range(NUM_BLOCKS)],
+        )
+        self.lnorm = nn.LayerNorm(EMBED_DIM)
+        self.linear = nn.Linear(EMBED_DIM, VOCAB_SIZE)
 
     def forward(self, input, target=None):
         loss = None
         B, T = input.shape
 
-        token_emb = self.token_embedding(input)  # B, T -> B, T, E
-        pos_emb = self.positional_embedding(torch.arange(T, device=DEVICE))  # T, E
+        token_emb = self.token_embed(input)  # B, T -> B, T, E
+        pos_emb = self.pos_embed(torch.arange(T, device=DEVICE))  # T, E
         x = token_emb + pos_emb  # B, T, E
-        x = self.self_attention_head(x)  # B, T, H
+        x = self.blocks(x)
+        x = self.lnorm(x)
         logit = self.linear(x)  # B, T, V
 
         if target is None:
@@ -135,7 +201,7 @@ class BigramLM(nn.Module):
 
         return logit, loss
 
-    def generate(self, input, max_tokens=100):
+    def generate(self, input, max_tokens=1000):
         for _ in range(max_tokens):
             input_cond = input[:, -SEQ_LEN:]
             logit, _ = self(input_cond)
@@ -151,6 +217,10 @@ model = BigramLM()
 model.to(DEVICE)
 optimizer = torch.optim.AdamW(model.parameters())
 
+num_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
+print(f"Parameter count: {num_parameters}")
+print("Beginning training...")
+
 # Train
 for iter in range(MAX_ITER):
     X, y = get_batch("train")
@@ -165,12 +235,16 @@ for iter in range(MAX_ITER):
         )
 
 # Save model
-torch.save(model.state_dict(), f"./models/bigram-{MAX_ITER}-attn.pt")
+model_path = (
+    f"./models/bigram-{MAX_ITER}-mha-block-residual-lnorm-dropout-paper_params.pt"
+)
+torch.save(model.state_dict(), model_path)
+print(f"Model saved to {model_path}")
 
 # model = BigramLM()
-# model.load_state_dict(torch.load("./models/bigram-10000-attn.pt"))
+# model.load_state_dict(torch.load("./models/bigram-100000-mha-with-ff.pt"))
 # model.to(DEVICE)
 
 # Generate shakespeare
 context = torch.zeros((1, 1), dtype=torch.long, device=DEVICE)
-print(decode(model.generate(context, max_tokens=500).squeeze().tolist()))
+print(decode(model.generate(context).squeeze().tolist()))
